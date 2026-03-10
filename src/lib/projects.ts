f24091f88dd23649603d1dbfa191bb5c997dad8f
Marios Konnaris <mariokonnari@gmail.com>
2026-03-10 13:20:26 +0200

    Updated projects section with real projects

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -11,29 +11,30 @@ export type Project = {
 export const projects: Project[] = [
     {
     num: "01",
-        title: "Project one",
+        title: "SaaS Dashboard with Auth + CRUD",
         description:
-            "A short description of what this project does, what problem it solves, and what you learned building it. Keep it to 2-3 sentences.",
-        tags: ["React", "TypeScript", "Tailwind"],
-        github: "https://github.com/marioskonnaris/project-one",
-        live: "https://project-one.vercel.app",
+            "A full-stack SaaS dashboard that allows users to sign up, log in, and manage data in a PostgreSQL database hosted on Supabase. Includes role-based access (Admin/User), JWT authentication, and a modern frontend built with Vite.",
+        tags: ["Vite","React", "TypeScript", "TailwindCSS", "Node.js", "Express", "Prisma", "PostgreSQL", "JWT", "Bcrypt"],
+        github: "https://github.com/mariokonnari/SaaS-Dashboard-Auth-CRUD",
+        live: "https://saasdashboarddemo.netlify.app/",
         featured: true,
     },
     {
     num: "02",
-    title: "Project Two",
+    title: "AI Code Reviewer",
     description:
-      "A short description of what this project does, what problem it solves, and what you learned building it. Keep it to 2-3 sentences.",
-    tags: ["Next.js", "Node.js", "PostgreSQL"],
-    github: "https://github.com/marioskonnaris/project-two",
+      "A powerful web application that provides instant, AI-powered code reviews using Google's Gemini API. Get expert-level feedback on security, performance, readability, testing, and general best practices.",
+    tags: ["Next.js", "React", "TypeScript", "TailwindCSS", "AI Integration", "jsPDF"],
+    github: "https://github.com/mariokonnari/AI-code-reviewer",
+    live: "https://aicodereviewerdemo.vercel.app/"
     },
     {
     num: "03",
-    title: "Project Three",
+    title: "Amazon Clone",
     description:
-      "A short description of what this project does, what problem it solves, and what you learned building it. Keep it to 2-3 sentences.",
-    tags: ["React", "Firebase", "CSS Modules"],
-    github: "https://github.com/marioskonnaris/project-three",
-    live: "https://project-three.vercel.app",
+      "A full-featured e-commerce web application inspired by Amazon, using React, TypeScript, Tailwind CSS and Redux Toolkit. Includes a fully functional Cart and the items are from the Fake Store REST API. ",
+    tags: ["React", "TypeScript", "Tailwind CSS", "Redux Toolkit", "Vite", "REST API"],
+    github: "https://github.com/mariokonnari/Amazon-Clone",
+    live: "https://mariozon.vercel.app/",
     },
 ];
